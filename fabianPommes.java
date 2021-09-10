47c328ff82ed67ca7c24e3149fa7117439d3fe0a
Fabian <fabianlueers@web.de>
2021-09-10 09:14:35 +0200

    POMMES

---
+++ fabianPommes.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class fabianPommes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class fabianPommes extends Actor
+{
+    /**
+     * Act - do whatever the fabianPommes wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    }    
+}
